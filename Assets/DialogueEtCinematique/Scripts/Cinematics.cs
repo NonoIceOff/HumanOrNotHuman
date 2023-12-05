893f21736b2feeeb714c013e0d23e4b056041bb0
NonoIceOff <54319211+NonoIceOff@users.noreply.github.com>
2023-12-05 12:18:03 +0100

    Récupération du texte et possibilité de changement

--- Assets/DialogueEtCinematique/Scripts/Cinematics.cs
+++ Assets/DialogueEtCinematique/Scripts/Cinematics.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Cinematics : MonoBehaviour
 {
@@ -9,10 +11,13 @@ public class Cinematics : MonoBehaviour
     public Camera thirdPathCamera;
     public Camera fourPathCamera;
 
+    public TextMeshProUGUI txt;
+
     int i;
 
     private void Start()
     {
+        txt.text = "Caméra 1";
         i = 0;
         firstPathCamera.enabled = true;
         secondPathCamera.enabled = false;
@@ -28,18 +33,21 @@ public class Cinematics : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (i == 0) {
+                txt.text = "Caméra 2";
                 firstPathCamera.enabled = false;
                 secondPathCamera.enabled = true;
                 secondPathCamera = Camera.main;
             }
 
             if (i == 1) {
+                txt.text = "Caméra 3";
                 secondPathCamera.enabled = false;
                 thirdPathCamera.enabled = true;
                 thirdPathCamera = Camera.main;
             }
 
             if (i == 2) {
+                txt.text = "Caméra 4";
                 thirdPathCamera.enabled = false;
                 fourPathCamera.enabled = true;
                 fourPathCamera = Camera.main;
