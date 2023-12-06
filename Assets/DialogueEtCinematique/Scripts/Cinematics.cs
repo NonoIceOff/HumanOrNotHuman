4ac4cc4b7d95f95e01ef758d64348e4512672ef0
NonoIceOff <54319211+NonoIceOff@users.noreply.github.com>
2023-12-06 12:23:15 +0100

    Début de cinematique 2, delai entre cameras, changement de style de texte

--- Assets/DialogueEtCinematique/Scripts/Cinematics.cs
+++ Assets/DialogueEtCinematique/Scripts/Cinematics.cs
@@ -15,13 +15,22 @@ public class Cinematics : MonoBehaviour
 
     public bool enableCinematics;
 
+    public int delayInterDialogues;
+
+    int actualDelay;
+
     int i;
     int visibleratio;
+
     public string[] dialogues = {"dialogue 1", "dialogue 2", "dialogue 3", "dialogue 4"};
+
+    string actualDialogue;
+    int dialoguelenght;
 
     private void Start()
     {
         i = 0;
+        actualDelay = 0;
         if (enableCinematics == true) {
             firstPathCamera.enabled = true;
             //firstPathCamera = Camera.main;
@@ -31,52 +40,59 @@ public class Cinematics : MonoBehaviour
         fourPathCamera.enabled = false;
     }
 
+    private void changeCinematicScene() {
+        actualDialogue = dialogues[i];
+        dialoguelenght = actualDialogue.Length;
+        visibleratio = 0;
+
+        if (i == 0) {
+            firstPathCamera.enabled = false;
+            secondPathCamera.enabled = true;
+        }
+
+        if (i == 1) {
+            secondPathCamera.enabled = false;
+            thirdPathCamera.enabled = true;
+        }
+
+        if (i == 2) {
+            thirdPathCamera.enabled = false;
+            fourPathCamera.enabled = true;
+        }
+
+        if (i==3) {
+            enableCinematics = false;
+            firstPathCamera.enabled = false;
+            secondPathCamera.enabled = false;
+            thirdPathCamera.enabled = false;
+            fourPathCamera.enabled = false;
+            txt.text = "";
+        }
+
+        i += 1;
+        actualDelay = 0;
+    }
+
     private void Update()
     {
         if (enableCinematics == true) {
             string actualDialogue = dialogues[i];
             int dialoguelenght = actualDialogue.Length;
 
-            txt.text = actualDialogue.Substring(0, visibleratio/10);
-
-            if (visibleratio <= dialoguelenght*10) {
+            txt.text = actualDialogue.Substring(0, visibleratio/19);
+            if (visibleratio <= dialoguelenght*19) {
                 visibleratio += 5;
+            } else {
+                if (actualDelay <= delayInterDialogues) {
+                    actualDelay += 1;
+                } else {
+                    changeCinematicScene();
+                }
             }
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                actualDialogue = dialogues[i];
-                dialoguelenght = actualDialogue.Length;
-                visibleratio = 0;
-
-                if (i == 0) {
-                    firstPathCamera.enabled = false;
-                    secondPathCamera.enabled = true;
-                   // secondPathCamera = Camera.main;
-                }
-
-                if (i == 1) {
-                    secondPathCamera.enabled = false;
-                    thirdPathCamera.enabled = true;
-                   // thirdPathCamera = Camera.main;
-                }
-
-                if (i == 2) {
-                    thirdPathCamera.enabled = false;
-                    fourPathCamera.enabled = true;
-                   // fourPathCamera = Camera.main;
-                }
-
-                if (i==3) {
-                    enableCinematics = false;
-                    firstPathCamera.enabled = false;
-                    secondPathCamera.enabled = false;
-                    thirdPathCamera.enabled = false;
-                    fourPathCamera.enabled = false;
-                    txt.text = "";
-                }
-
-                i += 1;
+                changeCinematicScene();
             }
         }
     }
